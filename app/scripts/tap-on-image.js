aa4dd0d0a95237a98798939b244f68051d65544a
Oliver Zhang <[oli.zhang@gmail.com]>
2018-05-04 09:52:54 +0800

    fix bug with image

--- app/scripts/tap-on-image.js
+++ app/scripts/tap-on-image.js
@@ -12,16 +12,29 @@
 		}
 		return false;
 	}
+
+	function wrap_single(el, wrapper) {
+	    el.parentNode.insertBefore(wrapper, el);
+	    wrapper.appendChild(el);
+	}
+
 	var images = document.querySelectorAll('.story-body img, .story-image figure');
+	// FIXME: - Should fix this
 	for (var i=0; i<images.length; i++) {
 		var image = images[i];
 		var imageUrl = image.src || image.getAttribute('data-url') || '';
 		if (isInLink(image) === false && imageUrl !== '') {
-			var parentEl = image.parentNode;
-			if (parentEl !== undefined) {			
-				var imgEl = parentEl.innerHTML;
-				parentEl.innerHTML = '<a href="' + imageUrl + '" target="_blank">' + imgEl + '</a>';
-			}
+			// var parentEl = image.parentNode;
+			// if (parentEl !== undefined) {			
+			// 	var imgEl = parentEl.innerHTML;
+			// 	parentEl.innerHTML = '<a href="' + imageUrl + '" target="_blank">' + imgEl + '</a>';
+			// }
+
+			var linkWrapper = document.createElement('a');
+			linkWrapper.href = imageUrl;
+			linkWrapper.target = '_blank';
+			wrap_single(image, linkWrapper);
 		}
 	}
+	
 })();
