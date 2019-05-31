0baf10b229dc7fd7a67fc6a9b3abe2a9adff44e3
Oliver Zhang <[oli.zhang@gmail.com]>
2019-05-31 15:18:31 +0800

    clip;

--- app/scripts/clip.js
+++ app/scripts/clip.js
@@ -1,13 +1,31 @@
-    var isLove=false
-    function clip(ele){
+var isLove=false
+function clip(ele){
 
-        if (isLove===false) {
-            console.log('llllove')
-            ele.src = 'http://static.ftchinese.com/images/png/loveActive.png'
-            isLove = true
-        }else{
-            console.log('not llllove')
-            ele.src = 'http://static.ftchinese.com/images/png/love.png'
-            isLove = false
-        }
+    if (isLove===false) {
+        console.log('llllove')
+        ele.src = 'http://static.ftchinese.com/images/png/loveActive.png'
+        isLove = true
+    }else{
+        console.log('not llllove')
+        ele.src = 'http://static.ftchinese.com/images/png/love.png'
+        isLove = false
     }
+}
+function updateFav(url, storyid) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', url);
+  xhr.setRequestHeader('Content-Type', 'application/text');
+  xhr.onload = function() {
+      if (xhr.status === 200) {
+          var data = xhr.responseText;
+          if (data === 'ok' || data === '') {
+              return data;
+          }
+      } else if (xhr.status !== 200) {
+          //alert('Request failed.  Returned status of ' + xhr.status);
+      }
+  };
+  xhr.send(JSON.stringify({
+      storyid: storyid
+  }));
+}
