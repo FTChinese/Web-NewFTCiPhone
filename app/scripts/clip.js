b52b7b864e3834895bc7267ad0a6ce9cba6618db
Oliver Zhang <oli.zhang@gmail.comm>
2022-05-13 12:46:06 +0800

    clipping

--- app/scripts/clip.js
+++ app/scripts/clip.js
@@ -11,21 +11,23 @@ function clip(ele){
         isLove = false
     }
 }
-function updateFav(url, storyid) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', url);
-  xhr.setRequestHeader('Content-Type', 'application/text');
-  xhr.onload = function() {
-      if (xhr.status === 200) {
-          var data = xhr.responseText;
-          if (data === 'ok' || data === '') {
-              return data;
-          }
-      } else if (xhr.status !== 200) {
-          //alert('Request failed.  Returned status of ' + xhr.status);
-      }
-  };
-  xhr.send(JSON.stringify({
-      storyid: storyid
-  }));
+function updateFav(url, storyid, type) {
+    var saveType = (type || 'story').replace(/^photonews$/g, 'photo');
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Content-Type', 'application/text');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var data = xhr.responseText;
+            if (data === 'ok' || data === '') {
+                return data;
+            }
+        } else if (xhr.status !== 200) {
+            //alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send(JSON.stringify({
+        storyid: storyid,
+        type: saveType
+    }));
 }
